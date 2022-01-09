2668d6cbfc2d6d126b847b1ba921d8c646ef9476
RJustoX <rafael.justoscherer@gmail.com>
2022-01-08 22:55:26 -0300

    melhorando methods

--- server/service/contentService.js
+++ server/service/contentService.js
@@ -1,9 +1,35 @@
 const contentData = require('../data/contentData');
 
 exports.getVicioCategories = async function (vicioId) {
-    return (await contentData.getVicioCategories(vicioId)).rows;
+    const result = {
+        status: 0,
+        message: 'Não foi possivel carregar as categorias',
+    };
+
+    const categories = (await contentData.getVicioCategories(vicioId)).rows;
+
+
+    if (categories.length > 0) {
+        result.status = 1;
+        result.message = 'Categorias encontradas com sucesso!'
+        result.value = categories;
+    }
+
+    return result;
 }
 
 exports.getVicioReasons = async function (vicioId) {
-    return (await contentData.getVicioReasons(vicioId)).rows;
+    const result = {
+        status: 0,
+        message: 'Não foi possivel carregar os motivos',
+    };
+    const reasons = (await contentData.getVicioReasons(vicioId)).rows;
+
+    if (reasons.length > 0) {
+        result.status = 1;
+        result.message = 'Motivos encontrados com sucesso!'
+        result.value = reasons;
+    }
+
+    return result;
 };
